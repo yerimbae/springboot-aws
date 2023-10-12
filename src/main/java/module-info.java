976529f56b7db9edaf46b0da6283695d378463c5
yerimbae <rimyebae@naver.com>
2023-10-12 16:11:53 +0900

    테스트코드 작성 및 롬복설치/전환

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.rimi.boo.springbootaws {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.rimi.boo.springbootaws to javafx.fxml;
-    exports com.rimi.boo.springbootaws;
-}
